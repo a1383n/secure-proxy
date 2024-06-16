f54ae16eff4abe2063a1dfafde45b316112249e0
Amirsobhan Nafariyeh <amirsobhan1553@gmail.com>
2024-06-16 17:39:07 +0330

    Finalize

---
+++ database/seeders/DomainFilterSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\DomianFilterType;
+use App\Enums\FilterItemPatternType;
+use App\Models\DomainFilter;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DomainFilterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $filter = new DomainFilter(['name' => 'Bypass all', 'enabled' => true, 'type' => DomianFilterType::BYPASS]);
+        $filter->save();
+
+        $filter->items()->create([
+            'pattern_type' => FilterItemPatternType::WILDCARD,
+            'pattern' => '*'
+        ]);
+    }
+}
