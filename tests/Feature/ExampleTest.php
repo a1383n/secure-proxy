d5745b77fc997af0ca37f821f06b8d1a73d0c660
Amirsobhan Nafariyeh <amirsobhan1553@gmail.com>
2024-06-16 17:44:16 +0330

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertSuccessful();
+        $this->assertTrue($response->isSuccessful() || $response->isRedirection() && $response->isRedirect('/admin'));
     }
 }
